42f2afd938cdcf2f3de63aeee4a55d7efd5e1d45
marotech223 <141754158+marotech223@users.noreply.github.com>
2023-08-22 23:57:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
